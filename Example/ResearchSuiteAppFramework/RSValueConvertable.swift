3ac882dc044cbba0773308e0e5b00dbe8140fd02
jdkizer9 <jdkizer9@users.noreply.github.com>
2017-06-24 20:34:05 -0400

    dev updates

---
+++ Example/ResearchSuiteAppFramework/RSValueConvertable.swift
@@ -0,0 +1,15 @@
+//
+//  RSValueConvertable.swift
+//  ResearchSuiteAppFramework
+//
+//  Created by James Kizer on 6/23/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+public protocol RSValueConvertableProtocol: NSObjectProtocol {
+    
+    
+
+}
